d6ff17b03540cc0a546016593f844f168912ee73
Felipe Bergamaschi <midisoft.corporation@gmail.com>
2023-07-21 20:36:05 -0300

    feat: add new constant

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,5 @@
+export const extensions = ['.tsx', '.ts', '.jsx', '.js'];
 export const DEFAULT_DIR = 'src/app';
 export const DEFAULT_OUT_DIR = 'src';
-export const EXTENSIONS = ['tsx', 'ts', 'jsx', 'js'];
 export const CURRENT_DIR = process.cwd();
+export const ROOT_DIR = '/'
