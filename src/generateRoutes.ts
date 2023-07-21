2cebda023572873b97d7dac50a0f25b5ee6d619c
Felipe Bergamaschi <midisoft.corporation@gmail.com>
2023-07-21 11:36:46 -0300

    fix: files

--- src/generateRoutes.ts
+++ src/generateRoutes.ts
@@ -3,4 +3,6 @@ import { Log } from "./utils/log";
 
 export function generateRoutes({ dir, extensions }: ViteRouterPros) {
   Log.info('Routes generated')
+  Log.info(`dir: ${dir}`)
+  Log.info(`extensions: ${extensions}`)
 }
