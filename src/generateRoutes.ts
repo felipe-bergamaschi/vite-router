2f5dbf5ed99c73a003f0bdfffb78a278789281be
Felipe Bergamaschi <midisoft.corporation@gmail.com>
2023-07-21 22:29:00 -0300

    feat: add generate routes

--- src/generateRoutes.ts
+++ src/generateRoutes.ts
@@ -1,8 +1,15 @@
+import path from "path";
+import fs from "fs";
+import { minify } from "terser";
+import babel from "@babel/core";
+
 import type { IViteRouterPros } from "./types";
 import { findLayout } from "./utils/findLayout";
 import { getFilesWithExtensions } from "./utils/getFilesWithExtensions";
-import { Log } from "./utils/log";
 import { processFiles } from "./utils/processFiles";
+import { template } from "./template";
+import { Log } from "./utils/log";
+import { CURRENT_DIR } from "./constants";
 
 export function generateRoutes({ dir = '', outDir = '' }: IViteRouterPros) {
   const getFiles = getFilesWithExtensions(dir);
@@ -11,8 +18,30 @@ export function generateRoutes({ dir = '', outDir = '' }: IViteRouterPros) {
 
   const routes = findLayout(files, layouts);
 
-  console.log({ routes })
+  const generateTemplate = template(routes, layouts);
 
-  // Log.info(getFiles)
-  // Log.info(`outDir: ${outDir}`)
+  babel.transform(generateTemplate, {
+    filename: 'router.tsx',
+    presets: ['@babel/preset-react', '@babel/preset-typescript']
+  }, (err, result) => {
+    if (err) {
+      Log.error('Generate template error:', err);
+
+      return;
+    }
+
+    minify(result?.code || '')
+      .then((minifiedResult) => {
+        const fileName = 'router.jsx';
+        const filePath = path.join(CURRENT_DIR, outDir, fileName);
+        const result = minifiedResult.code || '';
+
+        fs.writeFileSync(filePath, result, 'utf-8');
+        Log.info(`Generated successfully`);
+      })
+      .catch((error) => {
+        Log.error('Generate template error:', error);
+      });
+  });
+
 }
---
+++ src/template/index.ts
@@ -0,0 +1,41 @@
+import { IRoute, File } from '../types'
+import { generateImports, generateLayoutImports, generateRoutes } from './utils'
+
+export function template(routes: IRoute[], layouts: File[]) {
+  return `import { lazy, Suspense } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+${generateImports(routes)}
+
+${generateLayoutImports(layouts)}
+
+function Error404() {
+  return <div>404</div>;
+}
+
+function Loading() {
+  return <div>Loading...</div>;
+}
+
+interface IAppRoutesProps {
+  custom404?: React.ReactNode;
+  customLoading?: React.ReactNode;
+}
+
+export function AppRoutes({ custom404: Custom404, customLoading: CustomLoading }: IAppRoutesProps) {
+  return (
+    <BrowserRouter>
+      <Suspense fallback={CustomLoading || <Loading />}>
+        <Routes>
+          ${generateRoutes(routes)}
+          <Route
+            path="*"
+            element={Custom404 || <Error404 />}
+          />
+        </Routes>
+      </Suspense> 
+    </BrowserRouter>
+  );
+}
+`;
+}
