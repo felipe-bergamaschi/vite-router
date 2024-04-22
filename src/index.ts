793aae77f04f1a18a1d7545743506222356453c5
Felipe Bergamaschi <midisoft.corporation@gmail.com>
2024-04-22 14:19:20 -0300

    feat: export use navigate

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ export * from "./router";
 export * from "./template";
 export * from "./types";
 export * from "./walk";
+export * from "./hooks/useNavigate";
 
 import { ViteRouter } from "./router";
 export default ViteRouter;
