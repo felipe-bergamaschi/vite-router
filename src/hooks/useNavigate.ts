875f0f15dfb9ffd5bc3c1a112bc718ba24b4e5df
Felipe Bergamaschi <midisoft.corporation@gmail.com>
2024-04-25 11:50:59 -0300

    feat: add useNavigate

--- src/hooks/useNavigate.ts
+++ src/hooks/useNavigate.ts
@@ -1,56 +1,43 @@
-import qs from "qs";
 import { useNavigate as useRRDNavigate } from "react-router-dom";
-import type routes from "./routes.json";
+import type { Routes } from "./routes";
 
-type RouteEntry = {
-	readonly [K: string]: Readonly<Record<string, string>> | null;
-};
-
-type ExtractRouteParams<T> = T extends Record<string, string>
-	? { [K in keyof T as K extends string ? K : never]: string }
-	: undefined;
-
-type DynamicRoutes<T extends RouteEntry> = {
-	[P in keyof T]: ExtractRouteParams<T[P]>;
-};
-
-type ITypedRoutes = DynamicRoutes<typeof routes>;
-
-type RouteParams<T extends keyof ITypedRoutes> =
-	ITypedRoutes[T] extends undefined
-		? { params?: Record<string, string> }
-		: ITypedRoutes[T] & { params?: Record<string, string> };
+type Options<T extends keyof Routes> = Routes[T] extends null
+	? { params?: Record<string, string> }
+	: { params?: Record<string, string> } & Routes[T];
 
 export function useNavigate() {
+	return navigate;
+}
+
+function navigate<T extends keyof Routes>(path: T, options?: Options<T>) {
+	let formattedPath = path as string;
+
+	if (options) {
+		const pathParams = formattedPath.match(/:\w+/g) || [];
+
+		for (const param of pathParams) {
+			const key = param.slice(1);
+
+			if (options[key as keyof typeof options]) {
+				formattedPath = formattedPath.replace(
+					param,
+					options[key as keyof typeof options] as string,
+				);
+			}
+		}
+	}
+
+	const queryParams = options?.params
+		? new URLSearchParams(options.params).toString()
+		: "";
+
+	formattedPath += queryParams ? `?${queryParams}` : "";
+
+	return navigateTo(formattedPath);
+}
+
+function navigateTo(path: string) {
 	const navigate = useRRDNavigate();
 
-	return function navigateTo<T extends keyof ITypedRoutes>(
-		path: T,
-		options?: RouteParams<T>,
-	): void {
-		const params = options ?? {};
-		let pathWithParams = path;
-
-		// @ts-ignore
-		if (path.includes(":")) {
-			// @ts-ignore
-			pathWithParams = path.replace(/\:[a-zA-Z]+/g, (match) => {
-				const paramKey = match.substring(1); // Remove o ':'
-				if (!(paramKey in params)) {
-					throw new Error(
-						`Missing parameter '${paramKey}' for route '${path}'`,
-					);
-				}
-				return params[paramKey as keyof typeof params] as string;
-			});
-		}
-
-		// @ts-ignore
-		const queryString = qs.stringify(params.params);
-		const finalPath = queryString
-			? `${pathWithParams}?${queryString}`
-			: pathWithParams;
-
-		navigate(finalPath);
-	};
+	navigate(path);
 }
